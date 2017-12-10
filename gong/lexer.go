70798610f0bbf3c7aee12df42ef0557642335d7b
Marcos Minond <minond.marcos@gmail.com>
2017-12-10 01:31:33 -0700

    rename

--- gong/lexer.go
+++ gong/lexer.go
@@ -1,4 +1,4 @@
-package lexer
+package gong
 
 import (
 	"fmt"
--- main.go
+++ main.go
@@ -3,28 +3,28 @@ package main
 import (
 	"fmt"
 
-	"github.com/minond/gong/lexer"
+	"github.com/minond/gong/gong"
 )
 
 func main() {
-	fmt.Println(lexer.Lex(` false "one two \"three\" four" 1 2 0b001 0xFFF 32 s true1 true fda  +-*^/ [.] . ##@ `))
-	fmt.Println(lexer.Lex(`  3`))
+	fmt.Println(gong.Lex(` false "one two \"three\" four" 1 2 0b001 0xFFF 32 s true1 true fda  +-*^/ [.] . ##@ `))
+	fmt.Println(gong.Lex(`  3`))
 	fmt.Println("\n\n")
-	fmt.Println(lexer.Lex(`0b0001 0b0101 &`))
+	fmt.Println(gong.Lex(`0b0001 0b0101 &`))
 	fmt.Println("\n\n")
-	fmt.Println(lexer.Lex(`0b0001      0xF               ^`))
+	fmt.Println(gong.Lex(`0b0001      0xF               ^`))
 	fmt.Println("\n\n")
-	fmt.Println(lexer.Lex(`(define add (lambda (x y) (+ x y)))`))
+	fmt.Println(gong.Lex(`(define add (lambda (x y) (+ x y)))`))
 	fmt.Println("\n\n")
-	fmt.Println(lexer.Lex(`  false "one"   `))
+	fmt.Println(gong.Lex(`  false "one"   `))
 	fmt.Println("\n\n")
-	fmt.Println(lexer.Lex(`  (a)  `))
+	fmt.Println(gong.Lex(`  (a)  `))
 	fmt.Println("\n\n")
-	fmt.Println(lexer.Lex(` a->b = "c" `))
+	fmt.Println(gong.Lex(` a->b = "c" `))
 	fmt.Println("\n\n")
-	fmt.Println(lexer.Lex(` 1/3 `))
+	fmt.Println(gong.Lex(` 1/3 `))
 	fmt.Println("\n\n")
-	fmt.Println(lexer.Lex(` 1//3 ; 1 // 3;1    //     2`))
+	fmt.Println(gong.Lex(` 1//3 ; 1 // 3;1    //     2`))
 	fmt.Println("\n\n")
-	fmt.Println(lexer.Lex(`3.1//0.4`))
+	fmt.Println(gong.Lex(`3.1//0.4`))
 }
