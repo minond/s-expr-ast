cbc5691720352cdfd50c29fc1d2d35bb0bc79288
Marcos Minond <minond.marcos@gmail.com>
2017-12-11 22:52:34 -0700

    mock repl

--- main.go
+++ main.go
@@ -1,39 +1,49 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 
 	"github.com/minond/gong/gong"
 )
 
 func show(prog string) {
 	fmt.Println(gong.Lex(prog))
-	fmt.Println("\n\n")
+	fmt.Print("\n\n")
 }
 
 func main() {
-	show(` false "one two \"three\" four" 1 2 0b001 0xFFF 32 s true1 true fda  +-*^/ [.] . ##@ `)
-	show(`  3`)
-	show(`0b0001 0b0101 &`)
-	show(`0b0001      0xF               ^`)
-	show(`(define add (lambda (x y) (+ x y)))`)
-	show(`  false "one"   `)
-	show(`  (a)  `)
-	show(` a->b = "c" `)
-	show(` 1/3 `)
-	show(` 1//3 ; 1 // 3;1    //     2`)
-	show(`3.1//0.4`)
-	show(`{1 2 3 4} [2 * print] map`)
+	// show(` false "one two \"three\" four" 1 2 0b001 0xFFF 32 s true1 true fda  +-*^/ [.] . ##@ `)
+	// show(`  3`)
+	// show(`0b0001 0b0101 &`)
+	// show(`0b0001      0xF               ^`)
+	// show(`(define add (lambda (x y) (+ x y)))`)
+	// show(`  false "one"   `)
+	// show(`  (a)  `)
+	// show(` a->b = "c" `)
+	// show(` 1/3 `)
+	// show(` 1//3 ; 1 // 3;1    //     2`)
+	// show(`3.1//0.4`)
+	// show(`{1 2 3 4} [2 * print] map`)
+	//
+	// show(`1 2
+	// 3
+	// 4
+	//
+	//
+	// 5`)
+	// show(`one "tw\"o" 'thr\'ee' '"""""""' "''''''''''"`)
+	// show(`iiii"i`)
+	// show(`0b0001 1x2`)
+	// show(`true || false ; true | false ; true && true ; true & true;`)
+	// show(`true||false;true|false;true&&true;true&true;`)
 
-	show(`1 2
-	3
-	4
+	reader := bufio.NewReader(os.Stdin)
 
-
-	5`)
-	show(`one "tw\"o" 'thr\'ee' '"""""""' "''''''''''"`)
-	show(`iiii"i`)
-	show(`0b0001 1x2`)
-	show(`true || false ; true | false ; true && true ; true & true;`)
-	show(`true||false;true|false;true&&true;true&true;`)
+	for {
+		fmt.Print("> ")
+		text, _ := reader.ReadString('\n')
+		show(text)
+	}
 }
